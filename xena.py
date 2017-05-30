db1c31994a56d6ceb881ca580a2416d09331f072
Yunhai Luo <yunhail@stanford.edu>
2017-05-29 22:42:47 -0700

    Add "update" and "deletes" for transformation; add directory structures for main pipeline.

--- xena.py
+++ xena.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import gzip
+import os
 import timeit
 
 import numpy as np
@@ -12,7 +13,9 @@ import pandas as pd
 
 import gdc
 
-def xena_matrix_update_RNA(file_dict, init_matrix = pd.DataFrame()):
+def xena_matrix_update_RNA(file_dict, init_matrix=pd.DataFrame(), 
+                           update=True, deletes=None, index_cols=None, 
+                           data_col=None):
     """Transformation (log2(x+1)) and data matrix assembly for RNA-seq data 
     (HTSeq - Counts, HTSeq - FPKM and HTSeq - FPKM-UQ).
     
@@ -22,6 +25,12 @@ def xena_matrix_update_RNA(file_dict, init_matrix = pd.DataFrame()):
         init_matrix: A Xena matrix loaded as a Pandas DataFrame which be 
             updated with new data. Default is an empty DataFrame, i.e. the 
             function will return a new matrix in Pandas DataFrame format.
+        update: A boolean switch decides whether a new column should overwrite 
+            a column in the matrix if it has an overlapping column name. 
+            Default is to overwrite the old column. If given False, the 
+            funciton will rename both old and new columns.
+        deletes: A list of strings which correspond to the name of columns to 
+            be deleted.
     Returns:
         A Pandas DataFrame for Xena matrix.
     """
@@ -30,21 +39,26 @@ def xena_matrix_update_RNA(file_dict, init_matrix = pd.DataFrame()):
     
     total_files = len(file_dict)
     new_matrix = init_matrix
+    if isinstance(deletes, list):
+        for column in deletes:
+            if column in new_matrix.columns:
+                new_matrix.drop(column, axis=1, inplace=True)
+            else:
+                message = ('Sample {} was not found in matrix. '
+                           'Nothing to delete.')
+                print(message.format(column))
     i = 0
     for key in file_dict:
         i = i + 1
         print('\rProcessing {}/{} file...'.format(i, total_files), end='')
         with gzip.open(file_dict[key], 'r') as f:
-            raw_col = pd.read_table(f, 
-                                    sep='\t', 
-                                    header=None, 
-                                    names=[key], 
+            raw_col = pd.read_table(f, sep='\t', header=None, names=[key], 
                                     index_col=0)
             transformed_col = np.log2(raw_col + 1)
-            new_matrix = new_matrix.merge(transformed_col, 
-                                          how='outer', 
-                                          left_index=True, 
-                                          right_index=True)
+            if update and (key in new_matrix.columns):
+                new_matrix.drop(key, axis=1, inplace=True)
+            new_matrix = new_matrix.merge(transformed_col, how='outer', 
+                                          left_index=True, right_index=True)
     print('\rAll {} files have been processed. '.format(total_files), end='')
     print('Data Transformation done.')
     return new_matrix
@@ -53,6 +67,8 @@ def main():
     """The main pipeline for importing GDC data into Xena.
     """
 
+    xena_root = os.path.abspath('.')
+    print('Current Xena root directory is {}'.format(xena_root))
     label_field = 'cases.samples.portions.analytes.aliquots.submitter_id'
     data_type_tuples = [('Gene Expression Quantification', 'HTSeq - Counts'),
                         ('Gene Expression Quantification', 'HTSeq - FPKM'),
@@ -66,7 +82,21 @@ def main():
     for project_id in project_ids_list:
         status = 'Start to work on project "{}".'
         print(status.format(project_id))
+        project_dir = os.path.join(xena_root, project_id)
+        try: 
+            os.makedirs(project_dir)
+        except OSError:
+            if not os.path.isdir(project_dir):
+                raise
         for (data_type, workflow_type) in data_type_tuples:
+            data_dir_name = '{}_{}'.format(data_type.replace(' ', ''),
+                                           workflow_type.replace(' ', ''))
+            data_dir = os.path.join(project_dir, data_dir_name)
+            try: 
+                os.makedirs(data_dir)
+            except OSError:
+                if not os.path.isdir(data_dir):
+                    raise
             query_dict = {'cases.project.project_id': project_id,
                           'data_type': data_type,
                           'analysis.workflow_type': workflow_type}
@@ -83,11 +113,9 @@ def main():
                 continue
             
             status = 'Start to download {} - {} for {} at {} sec.'
-            print(status.format(data_type, 
-                                workflow_type, 
-                                project_id, 
+            print(status.format(data_type, workflow_type, project_id, 
                                 timeit.default_timer() - start_time))
-            files_dict = gdc.download_data(file_ids_dict, rename_ext_num=3)
+            files_dict = gdc.download_data(file_ids_dict, dir_path=data_dir)
             status = 'Download finishes at {} sec.'
             if not files_dict:
                 print('Download failed.')
@@ -97,20 +125,18 @@ def main():
             print(status.format(timeit.default_timer() - start_time))
             
             status = 'Start to transform {} - {} data for {} at {} sec.'
-            print(status.format(data_type,
-                                workflow_type,
-                                project_id,
+            print(status.format(data_type, workflow_type, project_id,
                                 timeit.default_timer() - start_time))
-            new_matrix = xena_matrix_update_RNA(files_dict)
+            new_matrix = xena_matrix_update_RNA(files_dict, update=True)
             status = 'Data transformation finishes at {} sec.'
             print(status.format(timeit.default_timer() - start_time))
             
             status = 'Start to save new matrix at {} sec.'
             print(status.format(timeit.default_timer() - start_time))
-            new_matrix_name = '{}_{}_{}.tsv'.format(project_id, 
-                                                    data_type, 
+            new_matrix_name = '{}_{}_{}.tsv'.format(project_id, data_type, 
                                                     workflow_type)
-            new_matrix.to_csv(new_matrix_name, sep='\t')
+            new_matrix_path = os.path.join(project_dir, new_matrix_name)
+            new_matrix.to_csv(new_matrix_path, sep='\t')
             status = 'New matrix saved at {} sec.'
             print(status.format(timeit.default_timer() - start_time))
 
