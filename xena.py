f15bb4a45ccd9a07d619259dc8fcb30cc87aa830
Yunhai Luo <yunhail@stanford.edu>
2017-06-01 23:07:59 -0700

    Add pipeline for miRNA and SNV data.

--- xena.py
+++ xena.py
@@ -4,7 +4,6 @@
 from __future__ import division
 from __future__ import print_function
 
-import gzip
 import os
 import timeit
 
@@ -13,9 +12,33 @@ import pandas as pd
 
 import gdc
 
-def xena_matrix_update_RNA(file_dict, init_matrix=pd.DataFrame(), 
-                           update=True, deletes=None, index_cols=None, 
-                           data_col=None):
+def read_by_ext(filename, mode='r'):
+    """Automatically decide how to open a file which might be compressed.
+    
+    Uses the code from the "hook_compressed" function in the fileinput module.
+    
+    Args:
+        filename: Must contain proper extension indicating the compression 
+            condition.
+        mode: Default is 'r'.
+    
+    Returns:
+        A filehandle to be used with `with`.
+    """
+    
+    ext = os.path.splitext(filename)[1]
+    if ext == '.gz':
+        import gzip
+        return gzip.open(filename, mode)
+    elif ext == '.bz2':
+        import bz2
+        return bz2.BZ2File(filename, mode)
+    else:
+        return open(filename, mode)
+
+def xena_matrix_merge(file_dict, init_matrix=pd.DataFrame(), 
+                           update=True, deletes=None, header='infer', 
+                           index_col=0, usecols=None, log_transform=True):
     """Transformation (log2(x+1)) and data matrix assembly for RNA-seq data 
     (HTSeq - Counts, HTSeq - FPKM and HTSeq - FPKM-UQ).
     
@@ -51,10 +74,14 @@ def xena_matrix_update_RNA(file_dict, init_matrix=pd.DataFrame(),
     for key in file_dict:
         i = i + 1
         print('\rProcessing {}/{} file...'.format(i, total_files), end='')
-        with gzip.open(file_dict[key], 'r') as f:
-            raw_col = pd.read_table(f, sep='\t', header=None, names=[key], 
-                                    index_col=0)
-            transformed_col = np.log2(raw_col + 1)
+        with read_by_ext(file_dict[key]) as f:
+            raw_col = pd.read_table(f, sep='\t', header=header, 
+                                    names=['', key], index_col=index_col, 
+                                    usecols=usecols)
+            if log_transform:
+                transformed_col = np.log2(raw_col + 1)
+            else:
+                transformed_col = raw_col
             if update and (key in new_matrix.columns):
                 new_matrix.drop(key, axis=1, inplace=True)
             new_matrix = new_matrix.merge(transformed_col, how='outer', 
@@ -63,8 +90,8 @@ def xena_matrix_update_RNA(file_dict, init_matrix=pd.DataFrame(),
     print('Data Transformation done.')
     return new_matrix
 
-def main():
-    """The main pipeline for importing GDC data into Xena.
+def import_rna_seq():
+    """The main pipeline for importing GDC RNA-seq data into Xena.
     """
 
     xena_root = os.path.abspath('.')
@@ -101,6 +128,7 @@ def main():
                           'data_type': data_type,
                           'analysis.workflow_type': workflow_type}
             gdc_filter = gdc.and_eq_filter_constructor(query_dict)
+            #file_ids_dict = gdc.get_file_dict(gdc_filter, label_field)
             file_ids_list = gdc.get_files_uuids(gdc_filter)
             if not file_ids_list:
                 message = 'No {} - {} data for "{}" project.'
@@ -127,7 +155,7 @@ def main():
             status = 'Start to transform {} - {} data for {} at {} sec.'
             print(status.format(data_type, workflow_type, project_id,
                                 timeit.default_timer() - start_time))
-            new_matrix = xena_matrix_update_RNA(files_dict, update=True)
+            new_matrix = xena_matrix_merge(files_dict, header=None)
             status = 'Data transformation finishes at {} sec.'
             print(status.format(timeit.default_timer() - start_time))
             
@@ -140,5 +168,210 @@ def main():
             status = 'New matrix saved at {} sec.'
             print(status.format(timeit.default_timer() - start_time))
 
+def import_miRNA_isoform():
+    """The main pipeline for importing GDC miRNA isoform expression data into 
+    Xena.
+    """
+
+    xena_root = os.path.abspath('.')
+    print('Current Xena root directory is {}'.format(xena_root))
+    label_field = 'cases.samples.portions.analytes.aliquots.submitter_id'
+    data_type = 'Isoform Expression Quantification'
+    workflow_type = 'BCGSC miRNA Profiling'
+
+    project_ids_list = gdc.get_all_project_ids()
+    # Remove the line below to get RNA-Seq data for all 39 projects on GDC.
+    project_ids_list = ['TCGA-BRCA']
+
+    start_time = timeit.default_timer()
+    for project_id in project_ids_list:
+        status = 'Start to work on project "{}".'
+        print(status.format(project_id))
+        project_dir = os.path.join(xena_root, project_id)
+        try: 
+            os.makedirs(project_dir)
+        except OSError:
+            if not os.path.isdir(project_dir):
+                raise
+        data_dir_name = '{}_{}'.format(data_type.replace(' ', ''),
+                                       workflow_type.replace(' ', ''))
+        data_dir = os.path.join(project_dir, data_dir_name)
+        try: 
+            os.makedirs(data_dir)
+        except OSError:
+            if not os.path.isdir(data_dir):
+                raise
+        query_dict = {'cases.project.project_id': project_id,
+                      'data_type': data_type,
+                      'analysis.workflow_type': workflow_type}
+        gdc_filter = gdc.and_eq_filter_constructor(query_dict)
+        #file_ids_dict = gdc.get_file_dict(gdc_filter, label_field)
+        file_ids_list = gdc.get_files_uuids(gdc_filter)
+        if not file_ids_list:
+            message = 'No {} - {} data for "{}" project.'
+            print(message.format(data_type, workflow_type, project_id))
+            continue
+        file_ids_dict = gdc.label_files(file_ids_list, label_field)
+        if not file_ids_dict:
+            message = 'Fail to label {} - {} data for "{}" project.'
+            print(message.format(data_type, workflow_type, project_id))
+            continue
+        
+        status = 'Start to download {} - {} for {} at {} sec.'
+        print(status.format(data_type, workflow_type, project_id, 
+                            timeit.default_timer() - start_time))
+        files_dict = gdc.download_data(file_ids_dict, dir_path=data_dir)
+        status = 'Download finishes at {} sec.'
+        if not files_dict:
+            print('Download failed.')
+            message = 'No downloaded {} - {} data for "{}" project.'
+            print(message.format(data_type, workflow_type, project_id))
+            continue
+        print(status.format(timeit.default_timer() - start_time))
+        status = 'Start to transform {} - {} data for {} at {} sec.'
+        print(status.format(data_type, workflow_type, project_id,
+                            timeit.default_timer() - start_time))
+        new_matrix = xena_matrix_merge(files_dict, header=0, index_col=0, 
+                                       usecols=[1, 3])
+        status = 'Data transformation finishes at {} sec.'
+        print(status.format(timeit.default_timer() - start_time))
+        
+        status = 'Start to save new matrix at {} sec.'
+        print(status.format(timeit.default_timer() - start_time))
+        new_matrix_name = '{}_{}_{}.tsv'.format(project_id, data_type, 
+                                                workflow_type)
+        new_matrix_path = os.path.join(project_dir, new_matrix_name)
+        new_matrix.to_csv(new_matrix_path, sep='\t')
+        status = 'New matrix saved at {} sec.'
+        print(status.format(timeit.default_timer() - start_time))
+
+def main():
+    """The main pipeline for importing GDC miRNA expression data into Xena.
+    """
+
+    xena_root = os.path.abspath('.')
+    print('Current Xena root directory is {}'.format(xena_root))
+    label_field = 'cases.samples.portions.analytes.aliquots.submitter_id'
+    data_type = 'miRNA Expression Quantification'
+    workflow_type = 'BCGSC miRNA Profiling'
+
+    project_ids_list = gdc.get_all_project_ids()
+    # Remove the line below to get RNA-Seq data for all 39 projects on GDC.
+    project_ids_list = ['TCGA-BRCA']
+
+    start_time = timeit.default_timer()
+    for project_id in project_ids_list:
+        status = 'Start to work on project "{}".'
+        print(status.format(project_id))
+        project_dir = os.path.join(xena_root, project_id)
+        try: 
+            os.makedirs(project_dir)
+        except OSError:
+            if not os.path.isdir(project_dir):
+                raise
+        data_dir_name = '{}_{}'.format(data_type.replace(' ', ''),
+                                       workflow_type.replace(' ', ''))
+        data_dir = os.path.join(project_dir, data_dir_name)
+        try: 
+            os.makedirs(data_dir)
+        except OSError:
+            if not os.path.isdir(data_dir):
+                raise
+        query_dict = {'cases.project.project_id': project_id,
+                      'data_type': data_type,
+                      'analysis.workflow_type': workflow_type}
+        gdc_filter = gdc.and_eq_filter_constructor(query_dict)
+        #file_ids_dict = gdc.get_file_dict(gdc_filter, label_field)
+        file_ids_list = gdc.get_files_uuids(gdc_filter)[0:3]
+        if not file_ids_list:
+            message = 'No {} - {} data for "{}" project.'
+            print(message.format(data_type, workflow_type, project_id))
+            continue
+        file_ids_dict = gdc.label_files(file_ids_list, label_field)
+        if not file_ids_dict:
+            message = 'Fail to label {} - {} data for "{}" project.'
+            print(message.format(data_type, workflow_type, project_id))
+            continue
+        
+        status = 'Start to download {} - {} for {} at {} sec.'
+        print(status.format(data_type, workflow_type, project_id, 
+                            timeit.default_timer() - start_time))
+        files_dict = gdc.download_data(file_ids_dict, dir_path=data_dir)
+        status = 'Download finishes at {} sec.'
+        if not files_dict:
+            print('Download failed.')
+            message = 'No downloaded {} - {} data for "{}" project.'
+            print(message.format(data_type, workflow_type, project_id))
+            continue
+        print(status.format(timeit.default_timer() - start_time))
+        status = 'Start to transform {} - {} data for {} at {} sec.'
+        print(status.format(data_type, workflow_type, project_id,
+                            timeit.default_timer() - start_time))
+        new_matrix = xena_matrix_merge(files_dict, header=0, index_col=0, 
+                                       usecols=[0, 2])
+        status = 'Data transformation finishes at {} sec.'
+        print(status.format(timeit.default_timer() - start_time))
+        
+        status = 'Start to save new matrix at {} sec.'
+        print(status.format(timeit.default_timer() - start_time))
+        new_matrix_name = '{}_{}_{}.tsv'.format(project_id, data_type, 
+                                                workflow_type)
+        new_matrix_path = os.path.join(project_dir, new_matrix_name)
+        new_matrix.to_csv(new_matrix_path, sep='\t')
+        status = 'New matrix saved at {} sec.'
+        print(status.format(timeit.default_timer() - start_time))
+
+def import_snv():
+    """The main pipeline for importing GDC SNV data into Xena.
+    """
+
+    xena_root = os.path.abspath('.')
+    print('Current Xena root directory is {}'.format(xena_root))
+    data_type = 'Masked Somatic Mutation'
+
+    project_ids_list = gdc.get_all_project_ids()
+    # Remove the line below to get RNA-Seq data for all 39 projects on GDC.
+    project_ids_list = ['TCGA-BRCA']
+
+    start_time = timeit.default_timer()
+    for project_id in project_ids_list:
+        status = 'Start to work on project "{}".'
+        print(status.format(project_id))
+        project_dir = os.path.join(xena_root, project_id)
+        try: 
+            os.makedirs(project_dir)
+        except OSError:
+            if not os.path.isdir(project_dir):
+                raise
+        data_dir_name = data_type.replace(' ', '')
+        data_dir = os.path.join(project_dir, data_dir_name)
+        try: 
+            os.makedirs(data_dir)
+        except OSError:
+            if not os.path.isdir(data_dir):
+                raise
+        query_dict = {'cases.project.project_id': project_id,
+                      'data_type': data_type}
+        gdc_filter = gdc.and_eq_filter_constructor(query_dict)
+        file_ids_dict = gdc.get_file_dict(gdc_filter)
+        if not file_ids_dict:
+            message = 'Fail to label {} data for "{}" project.'
+            print(message.format(data_type, project_id))
+            continue
+        
+        status = 'Start to download {} for {} at {} sec.'
+        print(status.format(data_type, project_id, 
+                            timeit.default_timer() - start_time))
+        files_dict = gdc.new_download_data(file_ids_dict.keys(), 
+                                           dir_path=data_dir)
+        status = 'Download finishes at {} sec.'
+        if not files_dict:
+            print('Download failed.')
+            message = 'No downloaded {} data for "{}" project.'
+            print(message.format(data_type, project_id))
+            continue
+        print(status.format(timeit.default_timer() - start_time))
+
 if __name__ == '__main__':
-    main()
+    import_snv()
+#    main()
