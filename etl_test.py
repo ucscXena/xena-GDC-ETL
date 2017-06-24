3247a8dd0b64226920aa04ba4d694080b4729a50
Yunhai Luo <yunhail@stanford.edu>
2017-06-23 22:56:52 -0700

    Update test script.

--- etl_test.py
+++ etl_test.py
@@ -10,28 +10,35 @@ import timeit
 
 import xena
 
-start_time = timeit.default_timer()
-script_dir = os.path.dirname(os.path.abspath(__file__))
-work_dir = os.path.join(script_dir, r'gitignore')
-mode = 'download'
-xena.import_gdc(project='TCGA-BRCA', dataset_type='rna_counts', 
-                work_dir=work_dir, mode=mode)
-#xena.import_gdc(project='TCGA-CHOL', dataset_type='rna_counts', 
-#                work_dir=work_dir, mode=mode)
-#xena.import_gdc(project='TCGA-CHOL', dataset_type='rna_fpkm', 
-#                work_dir=work_dir, mode=mode)
-#xena.import_gdc(project='TCGA-CHOL', dataset_type='rna_fpkmuq', 
-#                work_dir=work_dir, mode=mode)
-#xena.import_gdc(project='TCGA-CHOL', dataset_type='mirna', 
-#                work_dir=work_dir, mode=mode)
-#xena.import_gdc(project='TCGA-CHOL', dataset_type='mirna_isoform', 
-#                work_dir=work_dir, mode=mode)
-#xena.import_gdc(project='TCGA-CHOL', dataset_type='masked_cnv', 
-#                work_dir=work_dir, mode=mode)
-end_time = timeit.default_timer()
-print('Finish in {} sec.'.format(end_time - start_time))
-#test_size_mb = 75.18 # TCGA-CHOL 6 types
-test_size_mb = 310.76 # TCGA-BRCA rna_couts
-total_size_mb = 23870
-est_time = total_size_mb / test_size_mb * (end_time - start_time) / 60 / 60
-print('Expect to process ({}) all data in {} hours.'.format(mode, est_time))
+def main():
+    start_time = timeit.default_timer()
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    work_dir = os.path.join(script_dir, 'gitignore')
+    matrix_dir = None
+#    work_dir = os.path.join(matrix_dir, 'TCGA-BRCA', 'HTSeq-Counts')
+    mode = 'both'
+    xena.import_gdc(project='TCGA-BRCA', dataset_type='rna_counts', 
+                    work_dir=work_dir, mode=mode, matrix_dir=matrix_dir)
+    test_size_mb = 310.76 # TCGA-BRCA rna_couts
+#    xena.import_gdc(project='TCGA-CHOL', dataset_type='rna_counts', 
+#                    work_dir=work_dir, mode=mode, matrix_dir=matrix_dir)
+#    test_size_mb = 11.22 # TCGA-CHOL rna_counts
+#    xena.import_gdc(project='TCGA-CHOL', dataset_type='rna_fpkm', 
+#                    work_dir=work_dir, mode=mode)
+#    xena.import_gdc(project='TCGA-CHOL', dataset_type='rna_fpkmuq', 
+#                    work_dir=work_dir, mode=mode)
+#    xena.import_gdc(project='TCGA-CHOL', dataset_type='mirna', 
+#                    work_dir=work_dir, mode=mode)
+#    xena.import_gdc(project='TCGA-CHOL', dataset_type='mirna_isoform', 
+#                    work_dir=work_dir, mode=mode)
+#    xena.import_gdc(project='TCGA-CHOL', dataset_type='masked_cnv', 
+#                    work_dir=work_dir, mode=mode)
+#    test_size_mb = 75.18 # TCGA-CHOL 6 types
+    end_time = timeit.default_timer()
+    print('Finish in {} sec.'.format(end_time - start_time))
+    total_size_mb = 23870
+    est_time = total_size_mb / test_size_mb * (end_time - start_time) / 60 / 60
+    print('Expect to process ({}) all data in {} hours.'.format(mode, est_time))
+
+if __name__ == '__main__':
+    main()
