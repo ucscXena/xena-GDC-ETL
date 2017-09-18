3d6cf6958fc7ea4a3956a3be6894da473601d08e
Yunhai Luo <yunhail@stanford.edu>
2017-09-17 22:35:37 -0700

    Update script example.

--- etl_example.py
+++ etl_example.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""A script for importing GDC data into Xena
+"""A example script for importing GDC data into Xena
+
+For a simple complete import, the script can be adapted from this example by 
+modifying 3 variables: root_dir, projects and xena_dtypes
 """
 
 # Ensure Python 2 and 3 compatibility
 from __future__ import division
 from __future__ import print_function
 
-import os
+import sys
 import timeit
+import traceback
 
 import gdc
 from xena_dataset import XenaDataset
 
 def main():
     start_time = timeit.default_timer()
+    
     root_dir = '/home/yunhai/gdc/imported_GDC'
-    projects = gdc.get_all_project_info().index.tolist()
-    # Convert unicode to str for python 2
-    projects = [str(x) for x in projects]
+    # Get all project_ids on GDC, and convert unicode to str for python 2
+    projects = [str(x) for x in gdc.get_all_project_info().index]
+    # Selected types of datasets for Xena
     xena_dtypes = ['htseq.counts', 'htseq.fpkm', 'htseq.fpkm-uq', 'mirna', 
                    'masked.cnv', 'muse.snv', 'mutect2.snv', 
                    'somaticsniper.snv', 'varscan2.snv']
+    
     counts = 0
     total_projects = len(projects)
     for project in projects:
@@ -33,10 +39,13 @@ def main():
             try:
                 cohort = XenaDataset(project, dtype, root_dir)
                 cohort.download_gdc().transform().metadata()
-            except:
-                print('No {} data for cohort {}.'.format(dtype, project))
-    end_time = timeit.default_timer()
+            except Exception as e:
+                print('No {} data for cohort {}.'.format(dtype, project), 
+                      file=sys.stderr)
+                print(e, file=sys.stderr)
+                traceback.print_exc(file=sys.stderr)
 
+    end_time = timeit.default_timer()
     print('Finish in {} hours.'.format((end_time - start_time) / 60 / 60))
 
 if __name__ == '__main__':
