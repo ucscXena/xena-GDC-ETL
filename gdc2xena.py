077184570de830566118f5d025df85d3c3722946
Yunhai Luo <yunhail@stanford.edu>
2017-10-25 13:51:07 -0700

    Aggregate codes.

--- gdc2xena.py
+++ gdc2xena.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""A example script for importing genomic data on GDC into Xena
+"""A script for importing data from GDC to Xena
 
+Supported types of data are genomic data, phenotype data and survival data. 
 For a simple complete import, the script can be adapted from this example by 
 modifying 3 variables: root_dir, projects and xena_dtypes. For example::
 
-    script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     root_dir = os.path.join(script_dir, 'gitignore', 'test')
     projects = ['TCGA-CHOL', 'TARGET-RT']
-    xena_dtypes = ['htseq_counts', 'htseq_fpkm', 'htseq_fpkm-uq', 'mirna']
+    xena_dtypes = ['htseq_counts', 'phenotype', 'survival']
 
 """
 
@@ -31,6 +32,10 @@ def main():
     xena_dtypes = ['htseq_counts', 'htseq_fpkm', 'htseq_fpkm-uq', 'mirna', 
                    'masked_cnv', 'muse_snv', 'mutect2_snv', 
                    'somaticsniper_snv', 'varscan2_snv']
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.join(script_dir, 'gitignore', 'test')
+    projects = ['TARGET-NBL']
+    xena_dtypes = ['phenotype']
     
     counts = 0
     total_projects = len(projects)
@@ -42,11 +47,19 @@ def main():
     logger = logging.getLogger('Xena-GDC-ETL')
     for project in projects:
         counts += 1
-        print('Importing [{}/{}] projects: {}'.format(counts, total_projects, 
-                                                      project))
+        msg = 'Importing [{}/{}] projects: {}'
+        print(msg.format(counts, total_projects, project))
         for dtype in xena_dtypes:
-            try:
+            if dtype == 'survival':
+                dataset = GDCSurvivalset(project, root_dir)
+            elif dtype == 'phenotype':
+                if project.startswith('TCGA'):
+                    dataset = TCGAPhenoset(project, root_dir)
+                if project.startswith('TARGET'):
+                    dataset = TARGETPhenoset(project, root_dir)
+            else:
                 dataset = GDCOmicset(project, dtype, root_dir)
+            try:
                 dataset.download().transform().metadata()
             except Exception:
                 msg = 'No {} data for cohort {}.'.format(dtype, project)
@@ -55,7 +68,9 @@ def main():
     logging.shutdown()
     
     end_time = timeit.default_timer()
-    print('Finish in {} hours.'.format((end_time - start_time) / 60 / 60))
+    m, s = divmod(int(end_time - start_time), 60)
+    h, m = divmod(m, 60)
+    print('Finish in {:d}:{:02d}:{:02d}.'.format(h, m, s))
 
 if __name__ == '__main__':
     if __package__ is None:
@@ -65,7 +80,11 @@ if __name__ == '__main__':
         )
         import gdc
         from xena_dataset import GDCOmicset
+        from xena_dataset import TCGAPhenoset, TARGETPhenoset
+        from xena_dataset import GDCSurvivalset
     else:
         from .. import gdc
         from ..xena_dataset import GDCOmicset
+        from ..xena_dataset import TCGAPhenoset, TARGETPhenoset
+        from ..xena_dataset import GDCSurvivalset
     main()
