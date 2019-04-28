b4f0b548b5e84daf86872f285fba3e34829ce574
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-04-29 03:11:28 +0530

    Add tests for CLI

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,5 +9,16 @@ class ParserTest(unittest.TestCase):
 
     def test_xena_eql(self):
         parsed = self.parser.parse_args(["xena-eql", "df1", "df2"])
-        self.assertEqual(parsed.df1, "df1")
-        self.assertEqual(parsed.df2, "df2")
+        assert parsed.df1 == "df1"
+        assert parsed.df2 == "df2"
+
+    def test_make_metadata(self):
+        parsed = self.parser.parse_args(["make-metadata", "-m",
+                                        "path/to/matrix", "-t", "datatype"])
+        assert parsed.matrix == "path/to/matrix"
+        assert parsed.datatype == "datatype"
+
+    def test_gdc_check_new(self):
+        parsed = self.parser.parse_args(["gdc_check_new",
+                                        "https://example.com/data.gz"])
+        assert parsed.url == "https://example.com/data.gz"
