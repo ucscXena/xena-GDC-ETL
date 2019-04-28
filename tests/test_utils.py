b07238b31101ffc801f08ab1d5a2e8cd9770b8a4
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-04-28 20:57:32 +0530

    Add tests for equal_matrices

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,12 @@
 import os
+from mock import patch, call
+try:
+    from StringIO import StringIO 
+except ImportError:
+    from io import StringIO
+from unittest import TestCase
+
+import pandas as pd
 
 from xena_gdc_etl import utils
 
@@ -9,3 +17,16 @@ def test_mkdir_p():
     assert os.path.isdir(input_) is True
     assert input_ in result
     os.rmdir(input_)
+
+
+@patch("sys.stdout", new_callable=StringIO)
+def test_equal_matrices(mocked_print):
+        utils.equal_matrices(
+            "tests/fixtures/df1.csv",
+            "tests/fixtures/df2.csv"
+        )
+        utils.equal_matrices(
+            "tests/fixtures/df1.csv",
+            "tests/fixtures/df3.csv"
+        )
+        assert mocked_print.getvalue() == "Not equal.\nEqual.\n"
