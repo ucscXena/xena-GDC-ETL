254f67a04f0c91ce7739564402d997f98818b9f9
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-04-29 20:10:38 +0530

    tests: Use pytest.fixture for better readability

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,7 @@ except ImportError:
     from io import StringIO
 
 import pandas as pd
+import pytest
 
 from xena_gdc_etl import utils
 
@@ -20,43 +21,40 @@ def test_mkdir_p():
     os.rmdir(input_)
 
 
+@pytest.fixture
 @patch("sys.stdout", new_callable=StringIO)
 def test_equal_matrices(mocked_print):
-    utils.equal_matrices(
-        "tests/fixtures/df1.csv",
-        "tests/fixtures/df2.csv",
-    )
-    utils.equal_matrices(
-        "tests/fixtures/df1.csv",
-        "tests/fixtures/df3.csv",
-    )
+    utils.equal_matrices("df1.csv", "df2.csv")
+    utils.equal_matrices("df1.csv", "df3.csv")
     assert mocked_print.getvalue() == "Not equal.\nEqual.\n"
 
 
+@pytest.fixture
 @patch.object(utils, 'time', Mock(wraps=time))
 def test_metadata():
     utils.time.gmtime.return_value = time.struct_time(
         (2019, 4, 28, 0, 0, 0, 0, 0, 0)
     )
-    path_to_df = "tests/fixtures/HTSeq-FPKM-UQ.csv"
+    path_to_df = "HTSeq-FPKM-UQ.csv"
     utils.metadata(path_to_df, "htseq_fpkm-uq")
-    with open("tests/fixtures/HTSeq-FPKM-UQ.csv.json", "r") as actual:
+    with open("HTSeq-FPKM-UQ.csv.json", "r") as actual:
         actual = json.load(actual)
-    os.unlink("tests/fixtures/HTSeq-FPKM-UQ.csv.json")
-    with open("tests/fixtures/HTSeq-FPKM-UQ.json", "r") as expected:
+    os.unlink("HTSeq-FPKM-UQ.csv.json")
+    with open("HTSeq-FPKM-UQ.json", "r") as expected:
         expected = json.load(expected)
     assert actual == expected
 
 
+@pytest.fixture
 def test_merge_xena():
-    path = "tests/fixtures/"
     name = "merged_GDC_TARGET-CCSK.tsv"
-    filelists = [path + "merge-xena1.csv", path + "merge-xena2.csv"]
+    filelists = ["merge-xena1.csv", "merge-xena2.csv"]
     cohort = "GDC TARGET-CCSK"
     datatype = "GDC_phenotype"
+    path = "tests/fixtures"
     outdir = path
     utils.handle_merge_xena(name, filelists, cohort, datatype, outdir)
-    with open(path + "MergedCohort04292019.GDC_phenotype.tsv", "r") as actual:
+    with open("MergedCohort04292019.GDC_phenotype.tsv", "r") as actual:
         actual = pd.read_csv(actual)
     with open(path + name, "r") as expected:
         expected = pd.read_csv(expected)
