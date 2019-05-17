4421ec22e7c4fbb39d829cf56b9caa2c89773af5
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-05-17 11:47:52 +0530

    Add tests

---
+++ tests/test_xena_dataset.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+import pytest
+
+from xena_gdc_etl import xena_dataset
+
+
+@pytest.mark.CI
+def test_download(capfd):
+    dataset = xena_dataset.GDCOmicset(
+        projects="TCGA-BRCA",
+        root_dir=r".",
+        xena_dtype="htseq_counts",
+    )
+    dataset.download(no_of_files=1)
+    files = next(os.walk("TCGA-BRCA/Raw_Data/htseq_counts"))[2]
+    assert len(files) == 1
+    shutil.rmtree("./TCGA-BRCA", ignore_errors=True)
