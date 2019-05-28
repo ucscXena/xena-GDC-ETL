cd664a0a5da5385a4b0efbeeccfb55e3e755571f
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-05-29 01:15:24 +0530

    xena_dataset: Add tests for read_biospecimen(), read_clinical() (#26)

---
+++ tests/test_xena_dataset.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from xena_gdc_etl import xena_dataset
+
+
+PATH = "tests/fixtures/xena_dataset/"
+
+def test_read_biospecimen():
+    file_name = PATH + "nationwidechildrens.org_biospecimen.TCGA-AR-A0TQ"
+    expected = xena_dataset.read_biospecimen(file_name + ".xml")
+    actual = pd.read_csv(file_name + ".csv", sep="\t")
+    expected.equals(actual)
+
+
+def test_read_clinical():
+    file_name = PATH + "nationwidechildrens.org_omf.TCGA-RW-A68A"
+    expected = xena_dataset.read_clinical(file_name + ".xml")
+    actual = pd.read_csv(file_name + ".csv", sep="\t")
+    expected.equals(actual)
