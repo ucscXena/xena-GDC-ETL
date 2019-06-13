b402f5595ac92f37d2753f445ceb4ac75bb0c906
Ayan Banerjee <ayanbanerjee7777@gmail.com>
2019-06-14 01:17:49 +0530

    utils.py: Include status_forcelist inside Retry

--- xena_gdc_etl/utils.py
+++ xena_gdc_etl/utils.py
@@ -186,6 +186,7 @@ def reduce_json_array(j):
 def requests_retry_session(
     retries=5,
     backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
     session=None,
 ):
     session = session or requests.Session()
@@ -194,6 +195,7 @@ def requests_retry_session(
         read=retries,
         connect=retries,
         backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
     )
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
