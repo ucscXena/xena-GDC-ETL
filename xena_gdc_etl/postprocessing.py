7bb6617cdf407ff7f00d46d89a91ec8a312c6941
Cally Lin <callyylin@gmail.com>
2024-08-22 10:51:12 -0700

    Add CMI-MPC and CMI-MBC postprocessing - drop 'RNA' and 'WES' at end of samples and Add status statements

--- xena_gdc_etl/postprocessing.py
+++ xena_gdc_etl/postprocessing.py
@@ -80,6 +80,8 @@ def postprocess(data_type, df, gdc_data):
         for column in df.columns:
             for key, value in gdc_data.items():
                 if column in value: 
+                    msg = '{} has been changed to {}.'
+                    print(msg.format(column, key))
                     df.rename(columns={column: key}, inplace=True)
         duplicated_cols = df.columns.duplicated()
         duplicated = {list(df.columns)[i] for i, val in enumerate(duplicated_cols) if val}
@@ -91,6 +93,8 @@ def postprocess(data_type, df, gdc_data):
         for index, row in df.iterrows():
             for key, value in gdc_data.items():
                 if index in value: 
+                    msg = '{} has been changed to {}.'
+                    print(msg.format(index, key))
                     df.rename(index={index: key}, inplace=True) 
         df.reset_index(inplace=True)
         duplicated_rows = df.duplicated()
@@ -108,21 +112,51 @@ def main():
     options = parser.parse_args()
     file_df = pd.read_csv(options.file, sep='\t')
     filename = os.path.basename(options.file)
+    project = options.project
+    datatype = options.datatype
     new_dir = os.path.join(os.path.dirname(os.path.dirname(options.file)), 'Postprocessed_Matrices')
+    print('{} data will be postprocessed for the following datatype: {}.'.format(project, datatype))
     if not os.path.exists(new_dir):
+        print('Creating new directory for postprocessed matrices ...')
         os.mkdir(new_dir)
-    if options.project == 'BEATAML1.0-COHORT':
-        if options.datatype.startswith('star_'):
+    if project == 'BEATAML1.0-COHORT':
+        if datatype.startswith('star_'):
             processed_df = file_df.set_index('Ensembl_ID')
+            file_df.set_index('Ensembl_ID', inplace=True)
             for column in file_df.columns:
+                msg = '{} has been changed to {}.'
+                print(msg.format(column, column[:-1]))
                 processed_df.rename(columns={column: column[:-1]}, inplace=True)
         else:
+            print('Old sample column:\n')
+            print(file_df['sample'])
             file_df['sample'] = file_df['sample'].str[:-1]
+            print('New sample column:\n')
+            print(file_df['sample'])
+            processed_df = file_df.set_index('sample')
+    elif project == 'CMI-MPC' or project == 'CMI-MBC':
+        if datatype.startswith('star_'):
+            processed_df = file_df.set_index('Ensembl_ID')
+            file_df.set_index('Ensembl_ID', inplace=True)
+            for column in file_df.columns:
+                msg = '{} has been changed to {}.'
+                print(msg.format(column, column[:-4]))
+                processed_df.rename(columns={column: column[:-4]}, inplace=True)
+        else: 
+            print('Old sample column:\n')
+            print(file_df['sample'])
+            file_df['sample'] = file_df['sample'].str[:-4]
+            print('New sample column:\n')
+            print(file_df['sample'])
             processed_df = file_df.set_index('sample')
     else: 
-        gdc_data = get_gdc_data(options.project)
-        processed_df = postprocess(options.datatype, file_df, gdc_data)
+        gdc_data = get_gdc_data(project)
+        processed_df = postprocess(datatype, file_df, gdc_data)
+    status = 'Postprocessed {} data is ready for {}.'
+    print(status.format(datatype, project))
     processed_df.to_csv(os.path.join(new_dir, filename), sep='\t')
+    print('Postprocessed matrix is saved at {}.'.format(os.path.join(new_dir, filename)))
+
 
 if __name__ == '__main__':
     main()
